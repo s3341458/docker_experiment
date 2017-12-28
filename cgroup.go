102e8f22f55830c7bab70a649614ac1bc71c746a
s3341458 <s3341458@student.rmit.edu.au>
2017-12-29 08:44:35 +1100

    write down testcode for cgroup

---
+++ cgroup.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+    "os/exec"
+    "path"
+    "os"
+    "fmt"
+    "io/ioutil"
+    "syscall"
+    "strconv"
+)
+
+const cgroupMemoryHierarchyMount = "/sys/fs/cgroup/memory/"
+
+func main () {
+    if os.Args[0] == "/proc/self/exe" {
+        fmt.Printf("current pid %d", syscall.Getpid())
+        fmt.Println()
+        cmd := exec.Command("sh", "-c", "stress --vm-bytes 200m --vm-keep -m 1")
+        cmd.SysProcAttr = &syscall.SysProcAttr{}
+        cmd.Stdin = os.Stdin
+        cmd.Stdout = os.Stdout
+        cmd.Stderr = os.Stderr
+        if err := cmd.Run(); err != nil {
+            fmt.Println(err)
+            os.Exit(1)
+        }
+    }
+
+    cmd := exec.Command("/proc/self/exe")
+
+    cmd.SysProcAttr = &syscall.SysProcAttr{
+        Cloneflags: syscall.CLONE_NEWUTS|  syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+    }
+
+    cmd.Stdin = os.Stdin
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+
+    if err := cmd.Start(); err != nil  {
+        fmt.Println(err)
+        os.Exit(1)
+    } else {
+        fmt.Printf("global pid %v", cmd.Process.Pid)
+        fmt.Println()
+        // create and mount to limit memoery cgroup
+        os.Mkdir(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit"), 755)
+        ioutil.WriteFile(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit", "tasks"),
+                         []byte(strconv.Itoa(cmd.Process.Pid)), 0644)
+        ioutil.WriteFile(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit", "memory.limit_in_bytes"),
+                         []byte("100m"), 0644)
+    }
+    cmd.Process.Wait()
+}
