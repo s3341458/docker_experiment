68c16b59c48dee1ee15b51ad0b11dd70eb441753
s3341458 <s3341458@student.rmit.edu.au>
2018-01-01 10:50:27 +1100

    fix the issue where mount namespace does not work properly on archlinux

--- docker-3.2/container/init.go
+++ docker-3.2/container/init.go
@@ -9,6 +9,7 @@ import (
 func RunContainerInitProcess(command string, args []string) error {
 	logrus.Infof("command %s", command)
 
+    syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, "")
 	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
 	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
 	argv := []string{command}
